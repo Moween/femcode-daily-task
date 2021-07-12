7dd088810509568b6c412008c0ab3a6b9c87237c
moween <moween1998@gmail.com>
2021-07-12 18:15:40 +0100

    Add pseudo code.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Collect user year of birth;
+// Calculate age;
+// Check that age is greater 18 and less than 50;
+// Calculate Bonus
+// age 18- 20 => 5% of salary + salary;
+// age 21- 30 => 5% of salary + salary;
+// age 31- 50 => 5% of salary + salary;
