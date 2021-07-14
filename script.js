29ff0e68ea21c8999641f55553a98f73cef4afe8
moween <moween1998@gmail.com>
2021-07-14 09:54:25 +0100

    Add day2 task question

--- script.js
+++ script.js
@@ -46,6 +46,23 @@ const calculateEmployeeBonus = () => {
 
 
 // Day 2 Task
+// ======
+// Create a function that accepts a parameter  "n" 
+
+// The function should print out a multiplication table for n only If n is a multiple of 3, 5 or 7
+
+// multiplication table range is 1- 20
+
+// Example : 
+
+// functionname(3) should return
+// 3*1=3
+// 3*2 =6
+// 3*3 =9
+// .
+// .
+// .
+// 3*20 = 60
 
 const multipleOfThree = (num) => {
   const arr = [];
